b8ff7245eed732466a7ed31d795ad0a1061110b6
Aleksandar Ristevski <nice@ristevski.me>
2021-07-04 10:51:36 +0100

    no more refs to previous package

--- spikes/ch-import.js
+++ spikes/ch-import.js
@@ -1,6 +1,10 @@
+/*
 const ch = require('companies-house-uk');
 const { Jurisdiction } = require('companies-house-uk/lib/dictionaries/constants');
 
 console.log(JSON.stringify(Jurisdiction, null, 4));
 
 console.log(ch.Hello('Z'));
+*/
+
+console.log('TEST');
