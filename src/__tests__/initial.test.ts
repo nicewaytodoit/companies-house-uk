1815657b103edfebb1f3abd544695b7ffafed6cc
Aleksandar Ristevski <nice@ristevski.me>
2021-07-04 07:39:23 +0100

    Setup npm package build scripts and configuring Tests

---
+++ src/__tests__/initial.test.ts
@@ -0,0 +1,9 @@
+import { Hello } from '../index';
+
+describe('# Initial Test', () => {
+
+    it('Should return text with name', () => {
+        expect(Hello('Z')).toBe('Hello World - Z');
+    })
+
+});
