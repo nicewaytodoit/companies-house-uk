8c87db183b914153300c3d4e14113256789b4631
Aleksandar Ristevski <nice@ristevski.me>
2021-07-04 10:42:45 +0100

    Spike test of installing package

---
+++ spikes/ch-import.js
@@ -0,0 +1,6 @@
+const ch = require('companies-house-uk');
+const { Jurisdiction } = require('companies-house-uk/lib/dictionaries/constants');
+
+console.log(JSON.stringify(Jurisdiction, null, 4));
+
+console.log(ch.Hello('Z'));
--- spikes/transform.js
+++ spikes/transform.js
@@ -7,26 +7,30 @@ const CR = "\n";
 
 const getVariableName = (key) => key.split(/[\-\_]+/).map(([first, ...rest]) => first.toUpperCase() + rest.join('')).join('');
 
-try {
-    const root = './api-enumerations-master';
-    const files = fs.readdirSync(root);
-
-    files.forEach((fileName) => {
-        const ymlFilePath = path.join(root, fileName);
-        const file = fs.readFileSync(ymlFilePath, 'utf8');
-        const doc = yaml.load(file);
-        
-        const importLine = `import { IDictionary } from "..";` + CR + CR;
-
-        const text = Object.keys(doc)
-                            .map((key) => `export const ${getVariableName(key)}: IDictionary = ${JSON.stringify(doc[key], null, 2)};`)
-                            .reduce((acc, val) => acc + val + CR + CR, importLine);
+const transformAllYamals = () => {
+    try {
+        const root = './api-enumerations-master';
+        const files = fs.readdirSync(root);
     
-        const baseName = path.basename(ymlFilePath).split('.').slice(0, -1).join('.')
-        const tsFilePath = path.join('./out', `${baseName}.ts`);
-        console.log(tsFilePath);
-        fs.writeFileSync(tsFilePath, text, 'utf8');
-    });
-} catch (e) {
-    console.log(e);
-}
+        files.forEach((fileName) => {
+            const ymlFilePath = path.join(root, fileName);
+            const file = fs.readFileSync(ymlFilePath, 'utf8');
+            const doc = yaml.load(file);
+            
+            const importLine = `import { IDictionary } from "..";` + CR + CR;
+    
+            const text = Object.keys(doc)
+                                .map((key) => `export const ${getVariableName(key)}: IDictionary = ${JSON.stringify(doc[key], null, 2)};`)
+                                .reduce((acc, val) => acc + val + CR + CR, importLine);
+        
+            const baseName = path.basename(ymlFilePath).split('.').slice(0, -1).join('.')
+            const tsFilePath = path.join('./out', `${baseName}.ts`);
+            console.log(tsFilePath);
+            fs.writeFileSync(tsFilePath, text, 'utf8');
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+transformAllYamals();
