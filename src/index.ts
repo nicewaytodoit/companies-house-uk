bf98545bca3f18c01cf8404f336dc1bf1aac6777
Aleksandar Ristevski <nice@ristevski.me>
2021-07-06 11:19:58 +0100

    Disclamer in README and rudimentary API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import superagent from 'superagent';
+
 export interface IDictionary {
     [prop: string]: string;
 }
@@ -6,5 +8,35 @@ export interface INestedDictionary {
 }
 
 export const Hello = (worldName: string) => `Hello World - ${worldName}`;
+
+export const CompaniesHouseAPI = function (apiKey: string) {
+    const API_KEY = apiKey;
+
+    const getHelper = (endPoint: string, query: superagent.SuperAgentRequest | {}, callback: any) => {
+        superagent
+            .get('https://api.companieshouse.gov.uk/' + endPoint)
+            .query(query)
+            .auth(API_KEY, '')
+            .end(callback);
+    };
+
+    const getByUrl = (urlPath: string, obj = {}) => new Promise((resolve, reject) => getHelper(urlPath, obj, (err: any, res: any) => (err!==null) ? reject(err) : resolve(res)));
+
+    const search = (item: any) => getByUrl('search/companies', { q: item });
+    const getCompanyData = (number: string) => getByUrl(`company/${number}`);
+    const getFilingHistory = (number: string) => getByUrl(`company/${number}/filing-history`);
+    const getOfficers = (number: string) => getByUrl(`company/${number}/officers`);
+    const getPersons = (number: string) => getByUrl(`company/${number}/persons-with-significant-control`);
+    const getAddress = (number: string) => getByUrl(`company/${number}/registered-office-address`);
+
+    return {
+        search,
+        getCompanyData,
+        getFilingHistory,
+        getOfficers,
+        getPersons,
+        getAddress,
+    };
+};
 
 export * from './dictionaries';
